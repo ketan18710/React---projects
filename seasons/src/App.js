bb7dd153765200a1c856f7246c46dcd81f94c149
Ketan Verma <ketan18710@gmail.com>
2020-03-26 19:21:59 +0530

    seasons app completed

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+class App extends React.Component{
+  constructor(props)
+  {
+    super(props)
+    this.state = {
+      lat : null,
+      errMsg : null
+    };
+    window.navigator.geolocation.getCurrentPosition(
+          (position) => {this.setState({lat : position.coords.latitude})},
+          (err) => {this.setState({errMsg : err.message})}
+    )
+  }
+  render()
+  {
+    if(!this.state.errMsg &&  this.state.lat){
+      return <div> Latitude : {this.state.lat}</div>;
+    }
+    if(this.state.errMsg && !this.state.lat){
+      return <div> Error : {this.state.errMsg}</div>;
+    }
+    return <div>Loading!</div>;
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
-
 export default App;
